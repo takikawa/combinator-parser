139f07b26a82a008948c1263754be6125df5b76c
Asumu Takikawa <asumu@ccs.neu.edu>
2013-09-03 20:49:33 -0400

    Update pkg deps

--- info.rkt
+++ info.rkt
@@ -2,5 +2,6 @@
 
 (define collection 'multi)
 (define deps '("base" "parser-tools-lib" "compatibility-lib" "scribble-lib"
-               "parser-tools-doc" "racket-doc" "at-exp-lib" "lazy"))
+               "parser-tools-doc" "racket-doc" "at-exp-lib" "lazy"
+               "scheme-lib"))
 
